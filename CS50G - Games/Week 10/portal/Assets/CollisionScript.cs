3d8da4b31499ff6d84ad41a829e0845ca2a25efc
The King Lion <kinglionkl250@gmail.com>
2024-07-02 18:10:56 -0400

    Add Week 10 problem set

---
+++ CS50G - Games/Week 10/portal/Assets/CollisionScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollisionScript : MonoBehaviour
+{
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // start text off as completely transparent black
+		text.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        text.gameObject.SetActive(true);
+    }
+}
